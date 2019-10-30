700d7d44f57b9142e9224034f04ec8247fe5e3e3
Kuryu86 <Kuryu86+github@gmail.com>
2019-10-31 06:25:48 +0900

    スタック数の上限の設定

--- main.c
+++ main.c
@@ -1,50 +1,59 @@
 #include <stdio.h>
 #include <stdlib.h>
-typedef struct _Stack_Node 
+typedef struct _Stack_Node
 {
 	int data;
-	struct _Stack_Node* Under;
-}Stack_Node;
-Stack_Node* PushStack(Stack_Node* Top,Stack_Node* Data){
+	struct _Stack_Node *Under;
+} Stack_Node;
+Stack_Node *PushStack(Stack_Node *Top, Stack_Node *Data)
+{
 	Data->Under = Top;
 	return Data;
 }
-Stack_Node* PopStack(Stack_Node* Top){
-	Stack_Node* Data = Top->Under;
+Stack_Node *PopStack(Stack_Node *Top)
+{
+	Stack_Node *Data = Top->Under;
 	printf("%d\n", Top->data);
 	free(Top);
 	return Data;
 }
-void ShowStack(const Stack_Node* Top)
+void ShowStack(const Stack_Node *Top)
 {
-	//const Stack_Node *Now = Top;
 	while (1)
 	{
+		if (Top == NULL)
+			break;
 		printf("%d", Top->data);
 		Top = Top->Under;
-		if(Top==NULL)
+		if (Top == NULL)
 			break;
 		printf(",");
 	}
 	printf("\n");
 }
-int main(int argc, char *argv[]) 
+int main(int argc, char *argv[])
 {
-	Stack_Node *Top=NULL;
+	Stack_Node *Top = NULL;
+	int stackNum = 0;
 	char str[7];
-	while(fgets(str, sizeof(str), stdin)){
+	while (fgets(str, sizeof(str), stdin))
+	{
 		int iData = atoi(str);
-		if(iData==0)
+		if (iData == 0)
 			ShowStack(Top);
-		else if (iData==-1)
-			Top=PopStack(Top);
-		else
+		else if (iData == -1)
+		{
+			Top = PopStack(Top);
+			stackNum--;
+		}
+		else if(stackNum<10)
 		{
 			Stack_Node *Data;
-			Data = 
-				(Stack_Node *)malloc(sizeof( Stack_Node));
+			Data =
+				(Stack_Node *)malloc(sizeof(Stack_Node));
 			Data->data = iData;
 			Top = PushStack(Top, Data);
+			stackNum++;
 		}
 	}
 
