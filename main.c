3975ee88da8c139e023cb1f2a8ae55e0b0fdc275
Kuryu86 <Kuryu86+github@gmail.com>
2019-10-31 05:06:44 +0900

    Stack_Node構造体を定義

--- main.c
+++ main.c
@@ -1,10 +1,15 @@
 #include <stdio.h>
+typedef struct _Stack_Node 
+{
+	int data;
+	struct _Stack_Node* Under;
+}Stack_Node;
 
 int main(int argc, char *argv[]) 
 {
-    char str[7];
-    
-    while(fgets(str, sizeof(str), stdin)){
+
+	char str[7];
+	while(fgets(str, sizeof(str), stdin)){
         printf("%s", str);
     }
 
