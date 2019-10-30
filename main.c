c10dd99721107b58b1e370043c1804d4af4a8280
Kuryu86 <Kuryu86+github@gmail.com>
2019-10-31 05:16:18 +0900

    PushStack関数の定義

--- main.c
+++ main.c
@@ -4,14 +4,17 @@ typedef struct _Stack_Node
 	int data;
 	struct _Stack_Node* Under;
 }Stack_Node;
-
+Stack_Node* PushStack(Stack_Node* Top,Stack_Node* Data){
+	Data->Under = Top;
+	return Data;
+}
 int main(int argc, char *argv[]) 
 {
-
 	char str[7];
 	while(fgets(str, sizeof(str), stdin)){
         printf("%s", str);
-    }
+	
+	}
 
     return 0;
 }
